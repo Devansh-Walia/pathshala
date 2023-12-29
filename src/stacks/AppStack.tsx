3dc66962c0bb037d3d70a41d632edee4fcf97c37
Devansh-Walia <devanshwalia9898@gmail.com>
2023-12-29 12:54:03 +0530

    Add Kids and Teachers screens to AppStack

--- src/stacks/AppStack.tsx
+++ src/stacks/AppStack.tsx
@@ -1,4 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { KidsScreen, TeachersScreen } from 'src/components/dashboard'
+import SplashScreen from 'src/components/spalshScreen'
+import { USER_ROLES } from 'src/utils/constants'
+import useMeQuery from 'src/utils/hooks/me'
 import Account from '../components/account'
 
 type Props = {}
@@ -6,8 +10,15 @@ type Props = {}
 const Tab = createBottomTabNavigator()
 
 const AppStack = (props: Props) => {
-  return (
+  const { me, isLoading } = useMeQuery()
+  const isAdmin = me?.user_role === USER_ROLES.ADMIN
+
+  return isLoading ? (
+    <SplashScreen />
+  ) : (
     <Tab.Navigator>
+      <Tab.Screen name="Kids List" component={KidsScreen} />
+      {isAdmin ? <Tab.Screen name="Teachers List" component={TeachersScreen} /> : null}
       <Tab.Screen name="Account" component={Account} />
     </Tab.Navigator>
   )
--- src/stacks/RootStack.tsx
+++ src/stacks/RootStack.tsx
@@ -2,9 +2,11 @@ import Auth from 'src/components/auth'
 import SplashScreen from 'src/components/spalshScreen'
 import { useSession } from 'src/utils/hooks/session'
 import AppStack from './AppStack'
+import useMeQuery from 'src/utils/hooks/me'
 
 const RootStack = () => {
   const { data: session, isLoading } = useSession()
+  useMeQuery()
 
   return isLoading ? <SplashScreen /> : session && session.user ? <AppStack key={session.user.id} /> : <Auth />
 }
