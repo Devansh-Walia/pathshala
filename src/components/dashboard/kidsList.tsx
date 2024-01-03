92c0d343f7eab96f39499c3c5044e6d5c3bd673c
Devansh-Walia <devanshwalia9898@gmail.com>
2024-01-03 13:24:31 +0530

    used basic table with useGetKids hook

--- src/components/dashboard/kidsList.tsx
+++ src/components/dashboard/kidsList.tsx
@@ -1,13 +1,14 @@
-import { Text, View } from 'react-native'
+import { ActivityIndicator, Text, View } from 'react-native'
+import useGetKids from 'src/utils/hooks/getKids'
+import Table from '../shared/table'
 
 type Props = {}
 
 const KidsScreen = (props: Props) => {
-  return (
-    <View>
-      <Text>KidsScreen</Text>
-    </View>
-  )
+  const { kids, isLoading, error } = useGetKids()
+  console.log(kids)
+
+  return <View>{isLoading ? <ActivityIndicator /> : <Table />}</View>
 }
 
 export default KidsScreen
